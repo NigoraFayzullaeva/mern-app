c7d115849f8de5c9f61c6655cca11277d84f09c9
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-03-01 21:07:11 +0500

    go

---
+++ BACKEND/routes/places-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const placesControllers = require('../controllers/places-controllers');
+
+const router = express.Router();
+
+router.get('/:pid', placesControllers.getPlaceById);
+
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
+
+router.post('/', placesControllers.createPlace);
+
+router.patch('/:pid', placesControllers.updatePlace);
+
+router.delete('/:pid', placesControllers.deletePlace);
+
+module.exports = router;

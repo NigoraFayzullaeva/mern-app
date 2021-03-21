1e33bd3a0ddb27841c62051bc6c205edf65a3546
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-03-21 19:46:33 +0300

    sending a login request

--- FRONTEND/src/user/pages/Auth.js
+++ FRONTEND/src/user/pages/Auth.js
@@ -60,11 +60,34 @@ const Auth = () => {
 
   const authSubmitHandler = async event => {
     event.preventDefault();
+    
+    setIsLoading(true);
 
     if (isLoginMode) {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong, please try again.');
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch('http://localhost:5000/api/users/signup', {
           method: 'POST',
           headers: {
@@ -81,7 +104,6 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message);
         }
-        console.log(responseData);
         setIsLoading(false);
         auth.login();
       } catch (err) {

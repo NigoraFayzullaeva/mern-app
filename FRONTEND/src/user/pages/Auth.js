2a0eccb50e6938a8491b8e475cf2ae4c9b3feb3a
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-03-27 12:17:33 +0300

    wiring fb

--- FRONTEND/src/user/pages/Auth.js
+++ FRONTEND/src/user/pages/Auth.js
@@ -67,8 +67,6 @@ const Auth = () => {
   const authSubmitHandler = async event => {
     event.preventDefault();
 
-    console.log(formState.inputs);
-
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
@@ -86,17 +84,15 @@ const Auth = () => {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append('email', formState.inputs.email.value);
+        formData.append('name', formState.inputs.name.value);
+        formData.append('password', formState.inputs.password.value);
+        formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value
-          }),
-          {
-            'Content-Type': 'application/json'
-          }
+          formData
         );
 
         auth.login(responseData.user.id);
